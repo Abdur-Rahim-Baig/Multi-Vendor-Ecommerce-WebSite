7d0e8456b42a5cf5d328106203a0cd93c5adf2ed
Abdur Rahim <rahimbaig6666@gmail.com>
2026-08-30 16:38:41 +0530

    made the sidebar responsive for system and mobile too and hide if screen is minimised

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import { getPayload } from "payload";
 import configPromise from "@payload-config";
 import { Category } from "@/payload-types";
+import { CustomCategory } from "./type";
 
 import { Navbar } from "./navbar";
 import { Footer } from "./footer";
@@ -26,19 +27,16 @@ const Layout =  async ({ children }: Props) => {
         exists: false,
       },
     },
+    sort: "name"
   });
 
-  const formattedData = data.docs.map((doc) => ({
+  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
     ...doc,
     subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
       ...(doc as Category),
+      subcategories: undefined,
     }))
   }));
-
-  console.log({
-    data,
-    formattedData,
-  });
 
   return (
     <div className="flex flex-col min-h-screen">
--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,17 +1,39 @@
-import { SearchIcon } from "lucide-react";
+"use client";
+
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+
 import { Input } from "@/components/ui/input";
+
+import { CustomCategory } from "../type";
+import { CategoriesSidebar } from "./categories-sidebar";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 interface props {
   disabled?: boolean;
+  data?: CustomCategory[]; // Add the appropriate type for your data if needed
 };
 
-export const SearchInput = ({ disabled }: props) => {
+export const SearchInput = ({ 
+  disabled ,
+  data = []
+}: props) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar data={data} open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
       <div className="relative w-full">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-400" />
           <Input className="pl-8" placeholder="Search Product" disabled={disabled}/>
       </div>
+      <Button
+      variant="elevated"
+      className="size-12 shrink-0 flex lg:hidden"
+      onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon />
+      </Button>
     </div>
   );
 };
